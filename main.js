25a5522ea781c88cc2d7783809669d4b9f35bf94
LindaKadz <achielinda44@gmail.com>
2018-10-09 12:08:45 +0300

    debugging

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator__keys')
+const display = document.querySelector('.calculator__display')
 
-keys.addEventListener('click', e => {
- if (e.target.matches('button')) {
-   const key = e.target
+keys.addEventListener('click', event => {
+ if (event.target.matches('button')) {
+   const key = event.target
    const action = key.dataset.action
+   const keyContent = key.textContent
+   const displayedNum = display.textContent
 
    if (
      action === 'add' ||
@@ -25,6 +28,18 @@ keys.addEventListener('click', e => {
 
    if (action === 'calculate') {
      console.log('equal key!')
+   }
+
+   if (!action) {
+     if (displayedNum === '0') {
+    display.textContent = keyContent
+     } else {
+    display.textContent = displayedNum + keyContent
+  }
+
+  if (action === 'decimal') {
+   display.textContent = displayedNum + '.'
+  }
    }
  };
 });
